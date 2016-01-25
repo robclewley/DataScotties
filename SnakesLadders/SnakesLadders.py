73243dc74b416fc42ccb8b684924f0b2a09919a3
Clewley <rclewley@ascnt.edu>
2016-01-25 18:48:24 -0500

    Update to move and run

--- SnakesLadders/SnakesLadders.py
+++ SnakesLadders/SnakesLadders.py
@@ -19,9 +19,29 @@ class State(object):
 class GameFSM(object):
     def __init__(self, n):
         self.all_states = []
+        self.position = 0
+        self.n = n 
         for ix in range(n+1):
             self.all_states.append(State(ix))
-
+            
+    def move(self, die):
+        """die is an integer
+        """
+        inter_pos = self.position + die
+        state_obj = self.all_states[inter_pos]
+        final_pos = state_obj.process()
+        self.position = final_pos
+        
+    def run(self):
+        print("Starting game!")
+        while self.position < self.n:
+            # roll die
+            die = roll()
+            # move based on die roll
+            self.move(die)
+            # record results
+        print("Game over!")
+        
 game = GameFSM(16)
 
 print(game.all_states)
