a38da80f302ede4cf980e4a1bb1ced76332188b2
robclewley <rob.clewley@gmail.com>
2016-02-29 18:48:36 -0500

    Updated with convert_records and multi_run functions and example usage information

--- SnakesLadders/csv_utils.py
+++ SnakesLadders/csv_utils.py
@@ -1,3 +1,15 @@
+"""
+Utilities for working with CSV files representing all game move data.
+
+EXAMPLE USAGE:
+
+all_data = multi_run(n)
+save_csv(all_data, filename)
+
+all_data = load_csv(filename)
+
+...  for a given number of runs, n, and a string filename parameter.
+"""
 import pandas as pd
 
 def save_csv(all_data, filename):
@@ -21,4 +33,28 @@ def load_csv(filename):
         # strip game number column, reset index to 0
         df = group[['die','kind', 'start', 'end']].reset_index(drop=True)
         all_data.append(df)
+    return all_data
+
+def convert_records(records):
+    """
+    records is a list of dicts
+    """
+    d = {'die': [],
+        'start': [],
+        'end': [],
+        'kind': []
+        }
+    for recdict in records:
+        for k in ('die', 'start', 'end', 'kind'):
+            d[k].append(recdict[k])
+    return pd.DataFrame(d)
+
+def multi_run(n):
+    """n is number of times to run game, where game is a globally defined GameFSM object.
+    Returns list of dataframes created from move records (using convert_records)
+    """
+    all_data = []
+    for i in range(n):
+        game.run()
+        all_data.append(convert_records(game.records))
     return all_data
